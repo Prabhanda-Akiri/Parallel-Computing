c93b1c3096a785974fa0a13bb069fbf9d77f11f9
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2018-08-29 15:32:15 +0530

    Add files via upload

---
+++ lab1/p3.c
@@ -0,0 +1,15 @@
+/*shared.c*/
+#include<stdio.h>
+#include<omp.h>
+
+int main()
+{
+	int x=0;
+
+	#pragma omp parallel shared(x)
+	{
+		int tid=omp_get_thread_num();
+		x=x+1;
+		printf("Thread [%d] value of x is %d\n",tid,x);
+	}
+}
